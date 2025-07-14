355e826f054d823fd86d003fd4dc69aadf3334a3
Lina <linachugainova0@gmail.com>
2025-07-14 15:08:37 +0500

    Connection to database

--- FirstMicroservice/Program.cs
+++ FirstMicroservice/Program.cs
@@ -2,11 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
-app.MapGet("/", () => "работает");
-app.MapGet("/hello", () => "Hello World!");
-
-app.MapGet("/foo", () => "foo");
-
 
 
 
