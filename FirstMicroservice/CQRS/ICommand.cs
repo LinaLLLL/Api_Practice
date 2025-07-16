5cb49a07de4b5bd8cf7ac03c5e4dd38f4e15ae69
Lina <linachugainova0@gmail.com>
2025-07-16 10:58:07 +0500

    Change port

--- FirstMicroservice/CQRS/ICommand.cs
+++ FirstMicroservice/CQRS/ICommand.cs
@@ -2,12 +2,10 @@
 {
     //команды, которые не возвращают значение
     public interface ICommand : ICommand<Unit>
-
     {
     }
     //команды, которые возвращают значение
     public interface ICommand<out TResponse> : IRequest<TResponse>
-
     {
     }
 }
