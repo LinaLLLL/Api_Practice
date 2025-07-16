808da683ebe5d98506fa994c7b49cce58ff4115f
Lina <linachugainova0@gmail.com>
2025-07-16 11:04:05 +0500

    Register Behavior

--- FirstMicroservice/Program.cs
+++ FirstMicroservice/Program.cs
@@ -1,13 +1,20 @@
+using FirstMicroservice.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 builder.Services.AddMarten(option => { option.Connection(connectionString); })
     .UseLightweightSessions().InitializeWith<InitializeBookDatabase>();
 
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));  
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddCarter();
 
