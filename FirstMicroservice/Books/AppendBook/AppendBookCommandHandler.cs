dd7bf124585f18a63c53458092929ce77a46d84d
Lina <linachugainova0@gmail.com>
2025-07-16 10:22:52 +0500

    Add AppendBook endpoint

--- FirstMicroservice/Books/AppendBook/AppendBookCommandHandler.cs
+++ FirstMicroservice/Books/AppendBook/AppendBookCommandHandler.cs
@@ -1,6 +1,36 @@
-﻿namespace FirstMicroservice.Books.AppendBook
+﻿
+namespace FirstMicroservice.Books.AppendBook
 {
-    public class AppendBookCommandHandler
+    //обработчик команды
+    public record AppendBookCommand(
+        string Title,
+        string Name,
+        string Description,
+        string ImageUrl,
+        decimal Price,
+        List<string> Category
+        ) : ICommand<AppendBookResult>;
+
+    public record AppendBookResult(Guid id);
+    public class AppendBookCommandHandler(IDocumentSession session)
+        : ICommandHandler<AppendBookCommand, AppendBookResult>
     {
+        public async Task<AppendBookResult> Handle(AppendBookCommand command, CancellationToken cancellationToken)
+        {
+            var book = new Book
+            {
+                Title = command.Title,
+                Name = command.Name,
+                Description = command.Description,
+                ImageUrl = command.ImageUrl,
+                Price = command.Price,
+                Category = command.Category
+            };
+
+            session.Store( book );
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new AppendBookResult(book.Id);
+        }
     }
 }
