52336b61c5d0f281f3aa3d634f743f84b27815c0
Lina <linachugainova0@gmail.com>
2025-07-16 11:38:15 +0500

    Add ValidationBehavior

--- FirstMicroservice/Behaviors/ValidationBehavior.cs
+++ FirstMicroservice/Behaviors/ValidationBehavior.cs
@@ -1,6 +1,29 @@
 ﻿namespace FirstMicroservice.Behaviors
 {
-    public class ValidationBehavior
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
     {
+        public async Task<TResponse> Handle(TRequest request,
+            RequestHandlerDelegate<TResponse> next, 
+            CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResult = await Task.WhenAll(
+                validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failres = validationResult
+                .Where(request => request.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToList();
+
+            if (failres.Any())
+            {
+                throw new Exception("Ошибка валидации");
+            }
+            return await next();
+
+        }
     }
 }
